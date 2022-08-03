7c82b893c9d33fed4e3fc1a2d62441c0e1228e81
Matthew_Cash <matthew@matthew-cash.com>
2022-08-02 18:10:40 -0700

    Move to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+repositories {
+    maven(url = "https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc"
+    }
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.velocitypowered:velocity-api:3.0.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.0.1")
+    implementation("net.kyori:adventure-text-minimessage:4.11.0")
+    implementation("org.java-websocket:Java-WebSocket:1.5.3")
+    implementation("javax.json:javax.json-api:1.1")
+}
+
+group = "com.matthewcash.network"
+version = "2.0.0"
+description = "Discord Messaging"
+java.sourceCompatibility = JavaVersion.VERSION_17
